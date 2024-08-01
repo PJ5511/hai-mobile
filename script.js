0cfdd5d170beb2b662b8bbb722bed4ab6911761a
HÁI <pj@hai-swimwear.com>
2024-08-01 15:35:50 +0900

    script.js

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ document.addEventListener("DOMContentLoaded", function() {
     const spinButton = document.getElementById('spinButton');
 
     const prizes = [
-        { name: '當筆折扣1000元', probability: 0.25 },
-        { name: '當筆95折優惠', probability: 0.75 },
-        { name: '品牌周邊小物', probability: 33 },
-        { name: '品牌精選首飾', probability: 33 },
-        { name: '極透氣銀離子包臀內褲', probability: 33 }
+        { name: '當筆折扣1000元', probability: 1 },
+        { name: '當筆95折優惠', probability: 4 },
+        { name: '寵愛之名 亮白黃金藻防曬霜', probability: 35 },
+        { name: '品牌周邊小物', probability: 25 },
+        { name: '品牌精選首飾', probability: 15 },
+        { name: '極透氣銀離子包臀內褲', probability: 20 }
     ];
 
     const prizeColors = ['#FFDDC1', '#FFABAB', '#FFC3A0', '#FF677D', '#D4A5A5', '#392F5A'];
@@ -40,18 +41,20 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     function spinWheel() {
-        const randomValue = Math.random() * 100;
-        if (randomValue <= 0.25) {
-            window.location.href = 'result1.html'; // 當筆折扣1000元
-        } else if (randomValue <= 1) {
-            window.location.href = 'result2.html'; // 當筆95折優惠
-        } else if (randomValue <= 34) {
-            window.location.href = 'result4.html'; // 品牌周邊小物
-        } else if (randomValue <= 67) {
-            window.location.href = 'result5.html'; // 品牌精選首飾
-        } else {
-            window.location.href = 'result6.html'; // 極透氣銀離子包臀內褲
+        const randomValue = Math.random() * totalProbability;
+        let accumulatedProbability = 0;
+        let selectedPrize = null;
+
+        for (let prize of prizes) {
+            accumulatedProbability += prize.probability;
+            if (randomValue <= accumulatedProbability) {
+                selectedPrize = prize.name;
+                break;
+            }
         }
+
+        localStorage.setItem('prize', selectedPrize);
+        window.location.href = 'result.html';
     }
 
     drawWheel();
